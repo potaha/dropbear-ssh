8e1e974c3c1ece9d7c20cd393cea05afb7910072
potaha <84473573+potaha@users.noreply.github.com>
2022-03-20 16:54:05 +0300

    Create ssh_install.sh

---
+++ ssh_install.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+clear
+cd /tmp
+echo "Dang cap nhat tai nguyen..."
+apt-get update  > /dev/null 2>&1
+echo ""
+echo -e "\033[33;4mDropBear Kuruluyor\033[0m"
+echo ""
+apt-get -y install dropbear ssh
+sed -i 's/NO_START\=1/NO_START\=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT\=22/DROPBEAR_PORT\=442/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS\=/DROPBEAR_EXTRA_ARGS\=\"\-p 442 \-p 80 \-p 8080 \-p 8484 \-p 143 \-p 109\"/g' /etc/default/dropbear
+sed -i 's/#PermitRootLogin prohibit\-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin without\-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+service dropbear start
+service sshd restart
+echo ""
+echo ""
+echo -e "\033[31;4mDropBear Kuruldu\033[0m"
+echo "Dropbear erisilebilen portlar: 442, 80, 8080, 8484, 143, 109"
+echo -e "\033[31;4m 1 kisi baglanabilir sadece\033[0m"
+echo ""
+echo ""
+echo -e "\033[33;4m1 Sistem yeniden baslatiliyor baglantiniz kesilecek.\033[0m"
+sleep 0.9 ; reboot
