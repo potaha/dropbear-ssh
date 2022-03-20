8b8d75c0f44cac250978c57f7b465218f31feef7
potaha <84473573+potaha@users.noreply.github.com>
2022-03-20 17:25:26 +0300

    Update ssh_install.sh

--- ssh_install.sh
+++ ssh_install.sh
@@ -9,7 +9,7 @@ echo ""
 apt-get -y install dropbear ssh
 sed -i 's/NO_START\=1/NO_START\=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT\=22/DROPBEAR_PORT\=442/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS\=/DROPBEAR_EXTRA_ARGS\=\"\-p 442 \-p 80 \-p 8080 \-p 8484 \-p 143 \-p 109\"/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS\=/DROPBEAR_EXTRA_ARGS\=\"\-p 442 \-p 443 \-p 80 \-p 8080 \-p 8484 \-p 143 \-p 109\"/g' /etc/default/dropbear
 sed -i 's/#PermitRootLogin prohibit\-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin without\-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
